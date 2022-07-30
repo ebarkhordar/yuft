bea9819458e5b2a1e65453d682aa1ede105067f8
Ehsan <barkhordar.me@gmail.com>
2022-07-30 14:57:25 +0430

    add create view and list view

--- yuft/labels/urls.py
+++ yuft/labels/urls.py
@@ -4,6 +4,6 @@ from yuft.labels import views
 
 urlpatterns = [
     path('<int:pk>', views.LabelDetailView.as_view(), name='label-detail'),
+    path('list', views.LabelListView.as_view(), name='label-list'),
     path('create', views.LabelCreateView.as_view(), name='label-create'),
-
 ]
--- yuft/labels/views.py
+++ yuft/labels/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.views import generic
 
 from yuft.labels.models import Label
@@ -5,12 +6,18 @@ from yuft.labels.models import Label
 
 class LabelCreateView(generic.CreateView):
     model = Label
-    template_name = 'labeling/create.html'
+    template_name = 'labels/create.html'
     fields = ['name', 'brand', 'owner', 'serial_number']
-    success_url = '/thanks'
+
+    def get_success_url(self):
+        return reverse('label-detail', kwargs={'pk': self.object.pk})
 
 
 class LabelDetailView(generic.DetailView):
     model = Label
+    template_name = 'labels/detail.html'
 
 
+class LabelListView(generic.ListView):
+    model = Label
+    template_name = 'labels/list.html'
