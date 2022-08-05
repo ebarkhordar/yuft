babe4b96664ec2941837a594a398faba4fc0680c
Ehsan Barkhordar <barkhordar.me@gmail.com>
2022-08-05 19:15:32 +0430

    remove comments

--- yuft/labels/views.py
+++ yuft/labels/views.py
@@ -6,36 +6,8 @@ from yuft.labels.models import Label
 from yuft.labels.serializers import LabelSerializer
 
 
-#
-# class LabelCreateView(generic.CreateView):
-#     model = Label
-#     template_name = 'labels/create.html'
-#     fields = ['name', 'brand', 'owner', 'serial_number']
-#
-#     def get_success_url(self):
-#         return reverse('label-detail', kwargs={'pk': self.object.pk})
-
-
 class LabelCreateView(CreateAPIView):
     serializer_class = LabelSerializer
-
-    # def perform_create(self, serializer):
-    #     hash_size = 8
-    #     while True:
-    #         try:
-    #             complete_url = serializer.validated_data['complete_url']
-    #             serializer.save(short_url=short_url)
-    #             break
-    #         except IntegrityError:
-    #             hash_size += 1
-    #             logger.exception('Short url is not unique')
-
-    # @staticmethod
-    # def create_short_url(complete_url, hash_size):
-    #     hasher = hashlib.sha1(complete_url.encode('utf-8'))
-    #     truncate_hash = base64.urlsafe_b64encode(hasher.digest()[:hash_size]).decode()
-    #     short_url = "https://bitly/" + truncate_hash
-    #     return short_url
 
 
 class LabelRetrieveView(RetrieveAPIView):
