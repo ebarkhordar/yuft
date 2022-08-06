98ddc88b57a5ea0375d62b39a13fb49f0dc78a4a
Ehsan Barkhordar <barkhordar.me@gmail.com>
2022-08-06 14:53:29 +0430

    add sign func

---
+++ yuft/labels/crypto.py
@@ -0,0 +1,79 @@
+import binascii
+
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+
+from cryptography.hazmat.primitives.asymmetric import padding
+
+
+def generate_keypair_files():
+    private_key = rsa.generate_private_key(
+
+        public_exponent=65537,
+
+        key_size=2048,
+
+    )
+    pem = private_key.private_bytes(
+
+        encoding=serialization.Encoding.PEM,
+
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+
+        encryption_algorithm=serialization.NoEncryption()
+
+    )
+    with open('keys/privkey.pem', 'wb') as pem_out:
+        pem_out.write(pem)
+    public_key = private_key.public_key()
+
+    pem = public_key.public_bytes(
+
+        encoding=serialization.Encoding.PEM,
+
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+
+    )
+
+    with open('keys/publickey.pem', 'wb') as pem_out:
+        pem_out.write(pem)
+
+
+with open("keys/privkey.pem", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+    )
+
+
+def sign_text(message: str):
+    message_bytes = message.encode()
+    signature = private_key.sign(
+        message_bytes,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    signature_string = binascii.hexlify(signature)
+    return signature_string
+
+
+def verify_signature(signature_string, message):
+    signature = binascii.unhexlify(signature_string)
+    public_key = private_key.public_key()
+    try:
+        public_key.verify(
+            signature,
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return True
+    except:
+        return False
--- yuft/labels/views.py
+++ yuft/labels/views.py
@@ -1,13 +1,21 @@
-from django.urls import reverse
-from django.views import generic
+import logging
+import json
 from rest_framework.generics import RetrieveAPIView, ListAPIView, CreateAPIView
 
+from yuft.labels.crypto import sign_text
 from yuft.labels.models import Label
 from yuft.labels.serializers import LabelSerializer
+
+logger = logging.getLogger(__name__)
 
 
 class LabelCreateView(CreateAPIView):
     serializer_class = LabelSerializer
+
+    def perform_create(self, serializer):
+        label_json_str = json.dumps(serializer.validated_data)
+        signature_string = sign_text(label_json_str)
+        serializer.save(signature=signature_string)
 
 
 class LabelRetrieveView(RetrieveAPIView):
